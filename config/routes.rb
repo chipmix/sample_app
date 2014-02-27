3c50b8ca051c104f49bcc7dc377db82034f0f0d0
Filipe Rodrigues <filipe.a.rodrigues@live.com.pt>
2014-02-27 17:42:30 +0000

    Routes Added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 SampleApp::Application.routes.draw do
+  get "stactic_pages/home"
+  get "stactic_pages/help"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
